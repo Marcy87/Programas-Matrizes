066368e910b7e7ea89841c6b01e67f943673064b
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-21 09:15:33 -0300

    Exercício05
    Fazer um programa para ler duas matrizes de números inteiros A e B, contendo de M linhas e N colunas cada. Depois,
    gerar uma terceira matriz C onde cada elemento desta é a soma dos elementos correspondentes das matrizes originais.
    Imprimir na tela a matriz gerada.
    Entrada:
    A entrada contém os valores de M e N, depois os valores da primeira matriz A, depois os valores da segunda matriz B,
    conforme exemplo.
    Saída:
    A saída contém os valores da matriz gerada C, conforme exemplo.
    
    Exemplo:
    Entrada            Saída
    2 3                   5 9 7
    3 5 2                5 13 9
    4 5 1
    2 4 5
    1 8 8

---
+++ ProgramMatrizes05/ProgramMatrizes05/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ProgramMatrizes05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entradas = Console.ReadLine().Split(' ');
+
+            int M = int.Parse(entradas[0]);
+            int N = int.Parse(entradas[1]);
+
+            int[,] A = new int[M, N];
+            int[,] B = new int[M, N];
+            int[,] C = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    B[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    C[i, j] = A[i,j] + B[i,j];  
+                    Console.Write(C[i, j] + " ");
+                }
+                Console.WriteLine();   
+            }
+        }
+    }
+}
