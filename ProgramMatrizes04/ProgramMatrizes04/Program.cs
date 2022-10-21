ef3d5b3d76b7f7a37b186207ac6dd601360c79e3
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-20 22:26:53 -0300

    Exeercício04
    Ler um inteiro N e uma matriz quadrada de ordem N. Mostrar a soma dos elementos acima da diagonal principal.
    Entrada:
    A entrada contém o valor N, depois os dados da matriz.
    Saída:
    A saída contém a soma dos elementos da diagonal principal.
    
    Exemplo:
    Entrada              Saída
    3                        12
    10 3 2
    5 15 7
    8 6 4

---
+++ ProgramMatrizes04/ProgramMatrizes04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ProgramMatrizes04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    matriz[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+
+            int soma = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    soma += matriz[i, j];
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
